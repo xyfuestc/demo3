152aa3a8763e0f45d1b14f1bdee276eece0a4a0b
flyln <xyf_uestc@163.com>
2020-12-07 20:07:13 +0800

    添加了一个功能：compress

---
+++ b.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void compress(int a, int b)
+{
+	printf("%d",a+b);
+}
--- main.c
+++ main.c
@@ -6,3 +6,7 @@ int main()
 	compress(a,b);
 	return 0;
 }
+
+void compress(int a, int b){
+     printf("%d+%d=%d", a, b , a+b);
+}
